de4da2289232ef97b8b4c0040f439d6352747a9a
MR.Chang <86533019@qq.com>
2017-05-25 17:32:02 +0800

    https://github.com/changdaye/java8.git

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         Apple apple1 = new Apple();
         apple1.setWeoght("20");
-        apple1.setColor("fuck you");
+        apple1.setColor("fucking you");
 
         apples.add(apple);
         apples.add(apple1);
