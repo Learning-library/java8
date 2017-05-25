d10177a1fb22fa113a5e65c03f51ceb17e83e97b
changwenhu <wenhu.chang@smartapp4u.com>
2017-05-25 17:00:57 +0800

    https://github.com/changdaye/java8.git

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         Apple apple1 = new Apple();
         apple1.setWeoght("20");
-        apple1.setColor("dede");
+        apple1.setColor("dede111");
 
         apples.add(apple);
         apples.add(apple1);
