a69ee04b6d9bbed470b97121ebbe9d40568a2e85
changdaye <86533019@qq.com>
2017-05-25 17:01:48 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         Apple apple1 = new Apple();
         apple1.setWeoght("20");
-        apple1.setColor("dede111");
+        apple1.setColor("fuck");
 
         apples.add(apple);
         apples.add(apple1);
