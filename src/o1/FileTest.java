7f8eff7642a84fcc22b51e7c154a5c99c7ed31a3
MR.Chang <86533019@qq.com>
2017-06-21 23:05:41 +0800

    java8 in action 1 demo1

---
+++ src/o1/FileTest.java
@@ -0,0 +1,38 @@
+package o1;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+
+/**
+ * Created by changwenhu on 2017/6/21.
+ */
+public class FileTest {
+    @Test
+    public void test1() {
+        File[] files = new File(".").listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isHidden();//判断隐藏文件
+            }
+        });
+        for (File file : files) {
+            System.out.println(file.getName());
+        }
+    }
+
+    @Test
+    public void test2() {
+        Arrays.asList(
+                new File(".").listFiles(File::isHidden)
+        )
+                .stream()
+                .forEach(
+                        file -> System.out.println(file.getName())
+
+                );
+    }
+
+}
