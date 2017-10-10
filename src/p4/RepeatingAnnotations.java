03b36ce090870169e3a5319b36efdf9fdf8b00e5
MR.Chang <mrchang@jetbrains.org.cn>
2017-10-10 16:43:32 +0800

    Java语言的新特性 ： 2.4 重复注解

---
+++ src/p4/RepeatingAnnotations.java
@@ -0,0 +1,39 @@
+package p4;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author changwenhu
+ * @Date 2017/10/10
+ * @Blog https://jetbrains.org.cn/
+ * @Description ${todo}
+ */
+public class RepeatingAnnotations {
+    public static void main(String[] args) {
+        for (Filter filter : Filterable.class.getAnnotationsByType(Filter.class)) {
+            System.out.println(filter.value());
+        }
+    }
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Filters {
+        Filter[] value();
+    }
+
+    ;
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Repeatable(Filters.class)
+    public @interface Filter {
+        String value();
+    }
+
+    @Filter("filter1")
+    @Filter("filter2")
+    public interface Filterable {
+    }
+
+
+}
