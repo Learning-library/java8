2d2e37db9ff981f23768c517cc9607ca8a0e1982
MR.Chang <mrchang@jetbrains.org.cn>
2017-10-10 16:49:34 +0800

    Java语言的新特性 ： 2.5 更好的类型推断

---
+++ src/p5/TypeInference.java
@@ -0,0 +1,15 @@
+package p5;
+
+/**
+ * @Author changwenhu
+ * @Date 2017/10/10
+ * @Blog https://jetbrains.org.cn/
+ * @Description ${todo}
+ */
+public class TypeInference {
+    public static void main(String[] args) {
+        Value<String> stringValue = new Value<>();//这里给value 的泛型是String
+        String orDefault = stringValue.getOrDefault("123", Value.defaultValue());//这里我们看到 Value.defaultValue() 编译通过 编译联想到泛型是string 根据上下文
+        System.out.println(orDefault);
+    }
+}
---
+++ src/p5/Value.java
@@ -0,0 +1,18 @@
+package p5;
+
+/**
+ * @Author changwenhu
+ * @Date 2017/10/10
+ * @Blog https://jetbrains.org.cn/
+ * @Description ${todo}
+ */
+public class Value<T> {
+    public static <T> T defaultValue() {
+        return null;
+    }
+
+    public T getOrDefault(T value, T defaultValue) {
+        return (value != null) ? value : defaultValue;
+    }
+
+}
